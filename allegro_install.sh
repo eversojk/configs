b2ba856f312a0bc3aa36fb8f4f320d23c0681dbb
John Eversole <eversojk@gmail.com>
2014-02-25 14:59:33 -0500

    Fixed mkdir error if already exists

--- allegro_install.sh
+++ allegro_install.sh
@@ -6,15 +6,15 @@ sudo apt-get install build-essential git cmake cmake-curses-gui xorg-dev libgl1-
 sudo apt-get install libpng-dev libcurl4-openssl-dev libfreetype6-dev libjpeg-dev libvorbis-dev libopenal-dev libphysfs-dev libgtk2.0-dev libasound-dev libpulse-dev libflac-dev libdumb1-dev -y
 
 #git clone git://git.code.sf.net/p/alleg/allegro
-#cd allegro
-#git checkout 5.0
-#mkdir build
-#cd build
-#
-## hit c twice, then g, can choose any options
-#ccmake -DCMAKE_INSTALL_PREFIX=/usr ..
-#make -j4
-#sudo make install
-#
-#cd ../../
-#rm -rf allegro
+cd allegro
+git checkout 5.0
+mkdir -p build
+cd build
+
+# hit c twice, then g, can choose any options
+ccmake -DCMAKE_INSTALL_PREFIX=/usr ..
+make -j4
+sudo make install
+
+cd ../../
+rm -rf allegro
