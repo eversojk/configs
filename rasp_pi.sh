edf80324ea079e596b3c3a81238896320f36ae9a
John Eversole <eversojk@gmail.com>
2014-07-12 03:53:07 -0400

    Added dot files and git, pip

--- rasp_pi.sh
+++ rasp_pi.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-sudo apt-get install vim
+wget https://github.com/eversojk/scripts/archive/master.zip
+unzip master.zip
+cp scripts-master/.vimrc ~
+cp scripts-master/.bashrc ~
+cp scripts-master/.bash_aliases ~
+cp scripts-master/.gitconfig ~
+sudo rm -r scripts-master
+rm master.zip
+
+sudo apt-get install vim -y
+sudo apt-get install git -y
+sudo apt-get install python-pip -y
+
+sudo apt-get update
+
+sudo apt-get upgrade
