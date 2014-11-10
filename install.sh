9ec84e2c8268a257022c924a40f5863bd2050ab8
John Eversole <eversojk@gmail.com>
2014-11-10 17:07:02 -0500

    Added audacious

--- install.sh
+++ install.sh
@@ -60,5 +60,6 @@ sudo apt-get install redshift -y
 sudo apt-get install screen -y
 sudo apt-get install shutter -y
 sudo apt-get install redshift-gtk -y
+sudo apt-get install audacious -y
 
 sudo apt-get upgrade -y
