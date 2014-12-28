2f8ee090d66a8972e1cfd1fc8d14340468fe3dee
John Eversole <eversojk@gmail.com>
2014-12-28 00:03:48 -0500

    More improvements

--- install_arch.sh
+++ install_arch.sh
@@ -9,6 +9,8 @@ echo "blacklist pcspkr" | sudo tee -a /etc/modprobe.d/nobeep.conf
 
 sudo pacman -S --noconfirm zip unzip
 
+mkdir -p ~/.config
+mkdir -p ~/.i3
 curl -L -O https://github.com/eversojk/scripts/archive/master.zip
 unzip master.zip
 cp scripts-master/.bash_aliases ~
@@ -17,7 +19,6 @@ cp scripts-master/.gitconfig ~
 cp scripts-master/.vimrc ~
 cp scripts-master/redshift.conf ~/.config/
 sudo cp scripts-master/i3status.conf /etc/
-mkdir -p ~/.i3
 sudo cp scripts-master/i3_config ~/.i3/config
 sudo rm -r scripts-master
 rm master.zip
@@ -42,7 +43,9 @@ cd yaourt
 makepkg -s --noconfirm
 sudo pacman -U --noconfirm yaourt*.pkg.tar.xz
 
-# Install X
+rm -r builds
+
+ Install X
 sudo pacman -S xorg-server
 sudo pacman -S --noconfirm xorg-xinit
 sudo pacman -S --noconfirm xorg-setxkbmap
@@ -55,19 +58,22 @@ echo -e "[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacm
 sudo pacman -Syy
 sudo pacman -S --noconfirm nvidia
 sudo pacman -S --noconfirm lib32-nvidia-libgl
-# Load nvidia config
+ Load nvidia config
 echo "nvidia-settings --load-config-only &" | tee -a ~/.xinitrc
-# Set graphics card to max performance
+ Set graphics card to max performance
 echo "nvidia-settings -a [gpu:0]/GpuPowerMizerMode=1 &" | tee -a ~/.xinitrc
+ write xorg.conf
+sudo nvidia-xconfig
 
 # Install sound
-sudo pacman -S pulseaudio
-sudo pacman -S pulseaudio-alsa
+sudo pacman -S --noconfirm pulseaudio
+sudo pacman -S --noconfirm pulseaudio-alsa
+sudo pacman -S --noconfirm alsa-utils
 echo "start-pulseaudio-x11 &" | tee -a ~/.xinitrc
 echo "amixer sset Master unmute &" | tee -a ~/.xinitrc
 
 # Install time
-sudo pacman -S ntp
+sudo pacman -S --noconfirm ntp
 sudo systemctl enable ntpd.service
 
 # Misc
@@ -80,15 +86,16 @@ sudo pacman -S --noconfirm redshift
 sudo pacman -S --noconfirm steam
 sudo pacman -S --noconfirm vlc
 
-sudo yaourt -S --noconfirm spotify
+yaourt -S --noconfirm spotify
+yaourt -S --noconfirm google-chrome
 
-# Enable redshift on boot
-sudo systemctl /usr/lib/systemd/user/redshift-gtk.service
+## Enable redshift on boot
+#sudo systemctl /usr/lib/systemd/user/redshift-gtk.service
 
 # Start ssh-agent
-echo "eval $(ssh-agent) &" | tee -a ~/.xinitrc
+echo "eval \$(ssh-agent) &" | tee -a ~/.xinitrc
 
 # Install i3
-sudo pacman -S i3
-sudo pacman -S dmenu
+sudo pacman -S --noconfirm i3
+sudo pacman -S --noconfirm dmenu
 echo "i3" | tee -a ~/.xinitrc
