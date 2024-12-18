4347942c3875ce57688b6d4a58af74c23a446b44
John Eversole <2398285+eversojk@users.noreply.github.com>
2024-12-18 14:10:47 +0100

    Add most recent versions of nvim & tmux configs (#1)
    * Add LSP & treesitter
    
    * Add tmux
    
    * Fix whitespace
    
    * Remove unused lines from lsp-config.lua

--- conf/nvim/lua/vim-opts.lua
+++ conf/nvim/lua/vim-opts.lua
@@ -9,7 +9,6 @@ vim.opt.incsearch = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
-vim.opt.shiftwidth = 4
 vim.opt.showcmd = true
 vim.opt.smartcase = true
 vim.opt.tabstop = 4
