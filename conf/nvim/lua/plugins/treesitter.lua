06401251ddcf18926278ddfc77b1868bec23ad59
John Eversole <2398285+eversojk@users.noreply.github.com>
2024-12-18 14:10:47 +0100

    Add most recent versions of nvim & tmux configs (#1)
    * Add LSP & treesitter
    
    * Add tmux
    
    * Fix whitespace
    
    * Remove unused lines from lsp-config.lua

---
+++ conf/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        --require('nvim-treesitter.install').compilers = { "clang" }
+
+        configs.setup({
+            ensure_installed = { "lua", "vim", "cpp", "python" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
