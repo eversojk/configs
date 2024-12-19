d99b04138584acd162d88c4ac537cb42aa94c63c
John E <2398285+eversojk@users.noreply.github.com>
2024-12-19 16:31:38 +0100

    Add initial pass of debugging

--- conf/nvim/lua/plugins/debugging.lua
+++ conf/nvim/lua/plugins/debugging.lua
@@ -3,28 +3,52 @@ return {
     dependencies = {
         "nvim-neotest/nvim-nio",
         "rcarriga/nvim-dap-ui",
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
     },
     config = function()
         local dap = require("dap")
         local dapui = require("dapui")
 
-        dap.listeners.before.attach.dapui_config = function()
+        require("dapui").setup()
+
+        dap.adapters.lldb = {
+            type = "executable",
+            command = "/usr/bin/lldb-dap",
+            name = "lldb",
+        }
+
+        dap.configurations.cpp = {
+            {
+                name = "Launch",
+                type = "lldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = true,
+                args = {},
+            },
+        }
+
+        dap.configurations.c = dap.configurations.cpp
+
+        vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint)
+        vim.keymap.set("n", "<Leader>dc", dap.continue)
+        vim.keymap.set("n", "<Leader>dd", dap.disconnect)
+
+        local function open_dapui()
             dapui.open()
         end
 
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-
-        dap.listeners.before.event_terminated.dapui_config = function()
+        local function close_dapui()
             dapui.close()
         end
 
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-
-        vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint(), {})
-        --vim.keymap.set("n", "<Leader>dc", dap.continue(), {})
+        dap.listeners.before.attach.dapui_config = open_dapui
+        dap.listeners.before.launch.dapui_config = open_dapui
+        dap.listeners.before.event_terminated.dapui_config = close_dapui
+        dap.listeners.before.event_exited.dapui_config = close_dapui
     end,
 }
