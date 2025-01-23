7d2c423c93a71969d5150f038e3100f959c83375
eversojk <2398285+eversojk@users.noreply.github.com>
2025-01-23 17:11:03 +0100

    Add mason-null-ls to auto install formatters

--- conf/nvim/lua/plugins/none-ls.lua
+++ conf/nvim/lua/plugins/none-ls.lua
@@ -1,14 +1,25 @@
 return {
-    -- make sure to install stylua via the :Mason interface under Formatters
-    "nvimtools/none-ls.nvim",
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "williamboman/mason.nvim",
+        "nvimtools/none-ls.nvim",
+    },
     config = function()
-        local null_ls = require("null-ls")
+        local none_ls = require("null-ls")
+        local mason_none_ls = require("mason-null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format,
-            },
+        none_ls.setup()
+
+        local formatters_to_install = {
+            "stylua",
+            "clang_format",
+            "jq",
+        }
+
+        mason_none_ls.setup({
+            automatic_installation = true,
+            ensure_installed = formatters_to_install,
         })
 
         vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
