95349f08395837febdbd846757b2f301d803f1df
eversojk <2398285+eversojk@users.noreply.github.com>
2025-01-29 17:45:34 +0100

    Temporarily added additional telescope hotkeys

--- conf/nvim/lua/plugins/telescope.lua
+++ conf/nvim/lua/plugins/telescope.lua
@@ -20,6 +20,17 @@ return {
             vim.keymap.set("n", "<C-p>", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+            -- Marcin's stuff
+            vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+            vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+            vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+            vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+            vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+            vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] search existing buffers" })
+            vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find,
+                { desc = "[/] fuzzy search in current buffer" })
+            --
 
             require("telescope").load_extension("ui-select")
         end,
